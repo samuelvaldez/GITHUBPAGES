7c04d7ee64e1ca708ecc345fdcec0c87bb26b9bf
Samuel Valdez <savaldez@csumb.edu>
2020-09-25 00:31:21 -0700

    Added folder system for our app and also edited the index.js file to let clients see images/css/js files

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 const express = require('express');
 const app = express();
 const {readFile} = require('fs').promises;
+var path = require("path");
+var github = require('octonode');
+
+// Then we instantiate a client with or without a token (as show in a later section)
+// app.get('/chat.js', function(req, res){
+//     res.sendFile('js/scipts.js');
+// });
+app.use( express.static('public'));
 
 app.get('/', async (request, response) => {
+    
 
     readFile('./home.html', 'utf8', (err,html) => {
 
@@ -14,7 +23,11 @@ app.get('/', async (request, response) => {
        
     })
 
+   
+   
     response.send( await readFile('./home.html','utf8'));
 });
+//app.use('/img',express.static(path.join(__dirname, 'public/images')));
 
+//app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
 app.listen(process.env.PORT || 3000, () => console.log('App is availbe on http://localhost:3000'))
---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+//var github = require('octonode');
+
+
+
+
+function myFunction() {
+   //  var client = github.client();
+
+   //  client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+   //     // document.getElementById("demo").innerHTML = body;
+   //     console.log(body.login);
+   //  });\
+
+   alert("Hello World");
+  }
