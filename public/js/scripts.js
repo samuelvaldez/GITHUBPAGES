936abded3607b70752c85d972a4b057198d4635b
Samuel <savaldez@csumb.edu>
2020-09-25 21:05:30 -0700

    Installed nodemon to our code for we can just refresh our browser and not our server, changed html files in home and scripts to make sure user types something in instead of leaving the text box blank.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,27 +1,33 @@
 //var github = require('octonode');
-
-
-
-
-function myFunction() {
    //  var client = github.client();
 
    //  client.get('/users/pksunkara', {}, function (err, status, body, headers) {
    //     // document.getElementById("demo").innerHTML = body;
    //     console.log(body.login);
    //  });\
-   var message = $("#inputText").val();
-   var xhr = new XMLHttpRequest();
-    var data = message;
-    
-    xhr.open('GET', '/data/' +data);
-    xhr.onload = function(data) {
-        console.log('loaded', this.responseText);
-        $('#demo2').append(this.responseText);
-    };
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
+
+
+
+function myFunction() {
+
+  var message = $("#inputText").val();
+  var errorMessage = "Please Enter some text.";
+  if(message){
+  var xhr = new XMLHttpRequest();
+  var data = message;
+  
+  xhr.open('GET', '/data/' +data);
+  xhr.onload = function(data) {
+      console.log('loaded', this.responseText);
+      $('#demo2').empty().append(this.responseText);
+  };
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify(data));
+  } else {
+  $('#errorMessage').show();
   }
+   
+}
 
 
 
