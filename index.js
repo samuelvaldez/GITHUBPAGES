29eaea87cf612439821b1b5f63e44c11da7e9403
Samuel Valdez <savaldez@csumb.edu>
2020-09-27 20:03:49 -0700

    added all of code, cleaned up code and also fixed a few bugs

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ var bodyParser = require('body-parser');
 var client = github.client();
 var ghsearch = client.search();
 
-
 app.use( express.static('public'));
-
 app.use(bodyParser.json());
+
 //serves up home file 
 app.get('/', async (request, response) => {
     readFile('./home.html', 'utf8', (err,html) => {
@@ -24,40 +23,22 @@ app.get('/', async (request, response) => {
 });
 
 app.get('/data', function(req, res){
-    //console.log('body: ',  req.params.param1);
-    console.log(req.query.param2);
-    //var client = github.client();
-
-ghsearch.users({ q: req.query.param1 , sort: 'created',order: 'asc',per_page: 10, page: req.query.param2
+    ghsearch.users({ q: req.query.param1 , sort: 'created',order: 'asc',per_page: 10, page: req.query.param2
 }, function (err, data,headers) {
-
-    //console.log(data); //json object
-    const link = headers.link;
-    const links = link.split(",");
-    const urls = links.map(a=> {
-        return {
-            url: a.split(";")[0].replace(">","").replace("<",""),
-            title: a.split(";")[1]
-        }
-
-    })
-
+        const link = headers.link;
+        const links = link.split(",");
+        const urls = links.map(a=> {
+            return {
+                url: a.split(";")[0].replace(">","").replace("<",""),
+                title: a.split(";")[1]
+            }
+        })
     const newData = {
         jsonData: data,
         urlData: urls
     }
-
-
     res.send(JSON.stringify(newData));
-});
-    // ghsearch.get('/users?login='+req.params.param1, {}, function (err, status, body, headers) {
-       
-      
-    //   //res.send( req.params.param1);
-    // });
-
-    
-    
+    });
 });
 
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,60 +1,40 @@
-//var github = require('octonode');
-   //  var client = github.client();
-
-//const { user } = require("octonode");
-
-   //  client.get('/users/pksunkara', {}, function (err, status, body, headers) {
-   //     // document.getElementById("demo").innerHTML = body;
-   //     console.log(body.login);
-   //  });\
-
-
+//sends values to Server for it can make the api call, then recieve json data.
+//@param page contains the first page number this is passed to the api in which it gives me the first page
 function getSearch(page) {
- 
   var message = $("#inputText").val();
   var errorMessage = "Please Enter some text.";
   if(message){
-  var xhr = new XMLHttpRequest();
-  var data = message;
-
-  
-  xhr.open('GET', '/data?param1=' +data + '&param2=' + page,true);
-  xhr.onload = function(data) {
-    var data=this.responseText;
-    var jsonResponse = JSON.parse(data);
-    //console.log(jsonResponse);
-    separateData(jsonResponse['jsonData'], jsonResponse['urlData'])
-      //console.log(data,"\n");
-      //$('#demo2').empty().append(jsonResponse['jsonData']['total_count']);
-
-
-  };
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(data));
+    var xhr = new XMLHttpRequest();
+    var data = message;
+    xhr.open('GET', '/data?param1=' +data + '&param2=' + page,true);
+    xhr.onload = function(data) {
+      var data=this.responseText;
+      var jsonResponse = JSON.parse(data);
+      separateData(jsonResponse['jsonData'], jsonResponse['urlData'])
+    };
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
   } else {
-  $('#errorMessage').show();
+    $('#errorMessage').show();
   }
    
 }
 
-
+//separate data for i can use it easier
+//@param jsonRes is the whole api json call
+//@mapUrl contains meta data, url for last page and front page
 function separateData(jsonRes, mapUrl) {
-  console.log(jsonRes)
   var urlArr = parseUrl(mapUrl);
   parseData(jsonRes['items'], urlArr)
   showTotalCount(jsonRes['total_count'])
 }
 
+//Extract the page numbers from the url for I can use them later to navigate and paginate other pages.
+//@meta data url
 function parseUrl(mUrl){
-  //console.log(mUrl[0]['url'])
-
- 
-var reg = /\&page=[0-9]+/;
-
-
-var pat ;
-var final;
-
+  var reg = /\&page=[0-9]+/;
+  var pat ;
+  var final;
   var resArray = [];
   for(var item in mUrl) {
     pat = mUrl[item]['url'].match(reg);
@@ -62,68 +42,56 @@ var final;
     final = pat.split('=');
 
     resArray.push(final[1])
-    //console.log(mUrl)
     resArray.push(mUrl[item]['title'])
   }
-return resArray   
+  return resArray   
 }
 
 
-
+//Here we create our images,username and buttons for pagination
+//@jsonRes is the whole json 
+//@urlArr contains numbers and names for pagination
 function parseData(jsonRes, urlArr) {
   document.getElementById("divResults").innerHTML = "";
   for(var item in jsonRes){
-    
-    
-    
-    //console.log(jsonRes[item])
-    createUserName(jsonRes[item]['login'])
+    createUserName(jsonRes[item])
     createAvatar(jsonRes[item]['avatar_url'])
   }
-
   const divButton = document.getElementById("whereButtonsGo");
   divButton.innerHTML = "";
   var i;
-for (i = 0; i < urlArr.length; i=i+2) {
+  for (i = 0; i < urlArr.length; i=i+2) {
       const btn = document.createElement("button")
-      //console.log(urlArr[i])
       btn.textContent = urlArr[i+1];
       const num = parseInt(urlArr[i]);
       btn.addEventListener("click", e=> getSearch(num))
-      divButton.appendChild(btn);
-      
-}
-
- 
-
+      divButton.appendChild(btn); 
+  }
 
 }
 
-
+//create a username
 function createUserName(userName) {
-    const divExport = document.getElementById("divResults");
-    
-    var aTag = document.createElement('a');
-  aTag.setAttribute('href',"yourlink.htm");
-  aTag.innerText = userName;
- 
-    divExport.appendChild(aTag);
-
-
-
+  const divExport = document.getElementById("divResults");
+  var aTag = document.createElement('a');
+  aTag.setAttribute('href',userName['html_url']);
+  aTag.innerText = userName['login'];
+  divExport.appendChild(aTag);
   }
-  function createAvatar(imageUrl) {
-    const divExport = document.getElementById("divResults");
-    var x =  document.createElement("IMG");
-    x.setAttribute("src", imageUrl);
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    divExport.appendChild(x);
-  }
-
+//create image using the image url
+function createAvatar(imageUrl) {
+  const divExport = document.getElementById("divResults");
+  var x =  document.createElement("IMG");
+  x.setAttribute("src", imageUrl);
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  x.setAttribute("alt", "The Pulpit Rock");
+  divExport.appendChild(x);
+}
+//get the total count of results
 function showTotalCount(totalCount){
   const results = document.getElementById("results");
+  results.innerHTML = "";
   var h = document.createElement("H1");
   var t = document.createTextNode("RESULTS: " +totalCount);
   h.appendChild(t);
